f48778d1d5747203cca98aadd5ada611d781893c
tomikaa87 <tomikaa87@gmail.com>
2023-01-29 14:48:36 +0100

    Add info header to SunriseSunset

--- Software/LEDTimer.X/SunsetSunrise.h
+++ Software/LEDTimer.X/SunsetSunrise.h
@@ -1,9 +1,22 @@
 /*
- * File:   SunsetSunrise.h
- * Author: tkarpati
- *
- * Created on January 29, 2023, 10:41 AM
- */
+    This file is part of LEDTimer.
+
+    LEDTimer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LEDTimer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with LEDTimer.  If not, see <http://www.gnu.org/licenses/>.
+
+    Author: Tamas Karpati
+    Created on 2023-01-29
+*/
 
 #pragma once
 
